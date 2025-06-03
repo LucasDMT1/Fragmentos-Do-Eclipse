07f79a5ed32f68397a79976e7b77a598c711a133
LucasDMT <lucasvtalmeida@gmail.com>
2025-06-03 20:33:14 -0300

    adicionando movimentação com base em patrol point aos inimigos

--- enemies/enemy_ghost.gd
+++ enemies/enemy_ghost.gd
@@ -1,13 +1,29 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@export var speed = 1500
+@export var patrol_points : Node
+
 var GRAVITY = 1000
 const SPEED = 1500
 
 enum State {idle, walk}
 var current_state : State
 var direction : Vector2 = Vector2.LEFT
+var number_of_points : int
+var points_position : Array[Vector2]
+var current_point : Vector2
+var current_points_position : int
 
 func _ready():
+	if patrol_points != null:
+		number_of_points = patrol_points.get_children().size()
+		for point in patrol_points.get_children():
+			points_position.append(point.global_position)
+		current_point = points_position[current_points_position]
+	else:
+		print("No patrol points")
+	
 	current_state = State.idle
 
 func _physics_process(delta: float):
@@ -22,10 +38,26 @@ func enemy_falling(delta: float):
 		velocity.y += GRAVITY * delta
 		
 func enemy_idle(delta: float):
-	velocity.x = move_toward(velocity.x, 0, SPEED * delta)
+	velocity.x = move_toward(velocity.x, 0, speed * delta)
 	current_state = State.idle
 	
 func enemy_walk(delta: float):
-	velocity.x = direction.x * SPEED * delta
-	current_state = State.walk
+	
+	if abs(position.x -current_point.x) > 0.5:
+		velocity.x = direction.x * speed * delta
+		current_state = State.walk
+	else:
+		current_points_position += 1
+	
+	if current_points_position >= number_of_points:
+		current_points_position = 0 
+	
+	current_point = points_position[current_points_position]
+	
+	if current_point.x > position.x:
+		direction = Vector2.RIGHT
+	else:
+		direction = Vector2.LEFT
+	
+	animated_sprite_2d.flip_h = direction.x < 0
 	
