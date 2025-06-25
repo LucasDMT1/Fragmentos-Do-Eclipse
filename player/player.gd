de2b82516093bd82eccd4ae6345df47a505de3a2
IsabeleSilvestre <isabele.silvestre.27@gmail.com>
2025-06-25 19:12:27 -0300

    incluindo animação de morte do personagem

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hit_animation_player = $HitAnimationPlayer
-
+@onready var player_death_effect = $AnimatedSprite2D
 
 const GRAVITY = 1000
 @export var speed : int = 1000
@@ -89,9 +89,16 @@ func input_movement():
 	
 	return direction
 
+func player_death():
+	var player_death_effect_instance = player_death_effect.instance() as Node2D
+	player_death_effect_instance.global_position = global_position
+	get_parent().add_child(player_death_effect_instance)
+	queue_free()
 
 func _on_hurt_box_body_entered(body: Node2D):
 	if body.is_in_group("Enemy"):
 		print("Enemy entered ", body.damage_amount)
 		hit_animation_player.play("hit")
 		HealthManager.decrease_health(body.damage_amount)
+	if HealthManager.current_health == 0:
+		player_death()
