5a27c2afa1ed898c179c146d4072b53ad53ce125
IsabeleSilvestre <isabele.silvestre.27@gmail.com>
2025-06-25 16:01:02 -0300

    incluindo animação de vida

--- player/player.gd
+++ player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var hit_animation_player = $HitAnimationPlayer
+
 
 const GRAVITY = 1000
 @export var speed : int = 1000
@@ -91,4 +93,5 @@ func input_movement():
 func _on_hurt_box_body_entered(body: Node2D):
 	if body.is_in_group("Enemy"):
 		print("Enemy entered ", body.damage_amount)
+		hit_animation_player.play("hit")
 		HealthManager.decrease_health(body.damage_amount)
